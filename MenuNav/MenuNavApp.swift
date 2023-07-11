c716450ddbc8778be8c099b6fdc60780ccc5efd2
Mark Jeschke <markjeschke@yahoo.com>
2023-07-11 16:23:54 -0700

    Initial Commit

---
+++ MenuNav/MenuNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuNavApp.swift
+//  MenuNav
+//
+//  Created by Mark Jeschke on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
